b018a7ccd74add2eba4d0f6c509cd4ab260797f3
Sudhir kumar <58461255+kumarSudhirTCQ@users.noreply.github.com>
2022-02-24 09:25:32 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -11,7 +11,20 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(100, 50, 100) == 'NORMAL')
     self.assertTrue(typewise_alert.infer_breach(50, 50, 100) == 'NORMAL')
     self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
-
+    
+  '''test case for check_and_alert(alertTarget, batteryChar, temperatureInC)'''
+  def test_check_and_alert(self):
+    batteryChar = {'coolingType': 'PASSIVE_COOLING'}
+    status = typewise_alert.check_and_alert('TO_EMAIL', batteryChar, 36)
+    self.assertTrue(status == 1)
+        
+    batteryChar = {'coolingType': 'PASSIVE_COOLING'}
+    status = typewise_alert.check_and_alert('TO_EMAIL', batteryChar, -36)
+    self.assertTrue(status == 1)
+        
+    batteryChar = {'coolingType': 'PASSIVE_COOLING'}
+    status = typewise_alert.check_and_alert('TO_EMAIL', batteryChar, 25)
+    self.assertTrue(status == 1)
 
 if __name__ == '__main__':
   unittest.main()
