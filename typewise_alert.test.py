0d7d6d6f3d229227429fe6084df6acf3c3d78831
Sudhir kumar <58461255+kumarSudhirTCQ@users.noreply.github.com>
2022-02-24 09:40:11 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,17 +1,8 @@
 import unittest
 import typewise_alert
 
-
 class TypewiseTest(unittest.TestCase):
   
-  ''' test case for infer_breach(value, lowerLimit, upperLimit)'''
-  def test_infers_breach_as_per_limits(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-    self.assertTrue(typewise_alert.infer_breach(101, 50, 100) == 'TOO_HIGH')
-    self.assertTrue(typewise_alert.infer_breach(100, 50, 100) == 'NORMAL')
-    self.assertTrue(typewise_alert.infer_breach(50, 50, 100) == 'NORMAL')
-    self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
-    
   '''test case for check_and_alert(alertTarget, batteryChar, temperatureInC)'''
   def test_check_and_alert(self):
     batteryChar = {'coolingType': 'PASSIVE_COOLING'}
