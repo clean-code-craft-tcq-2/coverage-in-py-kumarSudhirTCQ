716958219f9a8b920ce262822376605e59818c3e
Sudhir kumar <58461255+kumarSudhirTCQ@users.noreply.github.com>
2022-02-24 15:29:56 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,4 +1,20 @@
 from notification import*
+
+coolingTypeLimit = {
+    'PASSIVE_COOLING': {
+        'lowerLimit': 0,
+        'upperLimit': 35
+    },
+    'HI_ACTIVE_COOLING': {
+        'lowerLimit': 0,
+        'upperLimit': 45
+    },
+    'MED_ACTIVE_COOLING': {
+        'lowerLimit': 0,
+        'upperLimit': 40
+    }
+}
+
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
     return 'TOO_LOW'
@@ -7,17 +23,8 @@ def infer_breach(value, lowerLimit, upperLimit):
   return 'NORMAL'
 
 def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
+  lowerLimit = coolingTypeLimit[coolingType]['lowerLimit']
+  upperLimit = coolingTypeLimit[coolingType]['upperLimit']
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
