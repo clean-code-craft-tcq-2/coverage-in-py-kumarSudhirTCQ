c1e2ff5a9d2afec8d8ecdd9fa32faa121cd5b23f
Sudhir kumar <58461255+kumarSudhirTCQ@users.noreply.github.com>
2022-02-24 09:58:43 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -4,6 +4,10 @@ import typewise_alert
 class TypewiseTest(unittest.TestCase):
   def test_check_and_alert(self):
     batteryChar = {'coolingType': 'PASSIVE_COOLING'}
+    status = typewise_alert.check_and_alert('TO_EMAIL', batteryChar, 36)
+    self.assertTrue(status == 1)
+    
+    batteryChar = {'coolingType': 'PASSIVE_COOLING'}
     status = typewise_alert.check_and_alert('TO_EMAIL', batteryChar, -36)
     self.assertTrue(status == 1)
     
